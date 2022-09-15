38b465e16b3b0e4075fe406553462fe30d5e7b10
Jin Igarashi <igarashi.jin@water-gis.com>
2022-09-15 13:02:04 +0100

    updated API url

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-	cactus_api: 'https://cactuscosting.com/cactus02/cactus-calcs/pull_cactus_json_app_json.php',
+	cactus_api: 'https://cactuscosting.com/cactus-calcs/pull_cactus_json_app_json.php',
 	attribution:
 		'&copy; <a href="https://cactuscosting.com" target="_blank" rel="noopener">CACTUS project</a>'
 };
